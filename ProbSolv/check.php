53622b76867ee35a4c695126b4234e8098c0775b
Iliya <55933990+CoDipper@users.noreply.github.com>
2020-06-16 17:00:25 +0300

    Update check.php

--- ProbSolv/check.php
+++ ProbSolv/check.php
@@ -1,4 +1,3 @@
-
 /*      implode()   Returns a string from the elements of an array
  *      explode()   Returns an array from the string inputt
  * Complete the timeConversion function below.
@@ -6,7 +5,12 @@
 function timeConversion($s) {
     $exploded= explode(":",$s);
     if(strcspn(exploded,"P")!=0){
-        if(exploded[0])
+        echo 
+        date("H:i:s",mktime(exploded[0],exploded[2],str_replace("PM","",exploded[3]),
+        0,0,0));
+    }else{
+        echo
+        date("h:i:s",mktime(exploded[0],exploded[2],str_replace("AM","",exploded[3]),
+        0,0,0));
     }
-
 }
