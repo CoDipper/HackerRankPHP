03867285b65d9013fef229060829989b504b6d4a
Iliya <55933990+CoDipper@users.noreply.github.com>
2020-06-16 14:37:08 +0300

    Update possibility.php

--- ProbSolv/possibility.php
+++ ProbSolv/possibility.php
@@ -1,3 +1,8 @@
+//the function takes an array of negative and possitive numbers as like: 
+// inPUT array:     -30, 2, -5, -3, 8, 0, 25, 0;
+// counts the possitive, negative and the zeros in different variables
+// so it can calculate the possibility of extracting each of them in a single try
+
 function plusMinus($arr) {
     $newArr= array(0.000000,0.000000,0.000000);
     $possCount=0;
