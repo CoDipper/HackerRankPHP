29706a0e2b9819cbc4cf9860599b0a3a1e85692f
Iliya <55933990+CoDipper@users.noreply.github.com>
2020-06-16 14:40:44 +0300

    Update and rename tiples.php to tipletes.php

--- ProbSolv/tipletes.php
+++ ProbSolv/tipletes.php
@@ -1,3 +1,6 @@
+//the function takes two arrays in which there is information about points for every round between two people- a and b;
+//then compares the points in every round so it can count the rounds won for each one of them
+
 function compareTriplets($a, $b) {
     $arr= array(0,0);
     foreach(array_combine($a, $b) as $a=>$b){
