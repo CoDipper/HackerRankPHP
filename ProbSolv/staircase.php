1ad6bb7acc9dcb2aa428ef92c336646369192bc7
Iliya <55933990+CoDipper@users.noreply.github.com>
2020-06-16 13:52:22 +0300

    Create staircase.php

---
+++ ProbSolv/staircase.php
@@ -0,0 +1,14 @@
+function staircase($n) {
+    for($i=$n; $i>0; $i--){
+
+        for($j=$i; $j>1; $j--){
+            echo " ";
+        }
+
+        for($z=0; $z<($n-$i+1); $z++){
+            echo "#";
+            
+        }
+        echo "\n";
+    }
+}
