5469196c7418aa4a35176e04eee8263e5d4a01e4
Iliya <55933990+CoDipper@users.noreply.github.com>
2020-06-16 16:43:33 +0300

    Create check.php

---
+++ ProbSolv/check.php
@@ -0,0 +1,12 @@
+
+/*      implode()   Returns a string from the elements of an array
+ *      explode()   Returns an array from the string inputt
+ * Complete the timeConversion function below.
+ */
+function timeConversion($s) {
+    $exploded= explode(":",$s);
+    if(strcspn(exploded,"P")!=0){
+        if(exploded[0])
+    }
+
+}
